9b96e2a9579dd22d61eb58bff97045e5be4c0037
Anathi9 <anathijosefu@gmail.com>
2024-02-07 13:58:46 +0200

    addition

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const {multiplication} = require('./multiplication')
 
 console.log('3 * 6 =', multiplication(3, 6));
+console.log('5 * 7 =' , multiplication(5, 7));
+console.log(addition(9,7));
+console.log(addition(8,8));
